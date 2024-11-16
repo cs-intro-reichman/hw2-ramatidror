37d268837efe24215349fbd2e0fc64c9187ac197
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-16 22:21:13 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,38 @@
-// Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+
+        String name = (args[0]).toUpperCase();
+        int nameLength = name.length();
+        int numberOfCheers = Integer.parseInt(args[1]);
+        String vowels = "AEFHILMNORSX";
+
+        for (int i = 0; i < nameLength; i++) {
+
+            if (vowels.indexOf(name.charAt(i)) != (-1)) {
+                System.out.println ("Give me an " + name.charAt(i) + ": " + name.charAt(i) + "!");
+            } else {
+                System.out.println("Give me a " + name.charAt(i) + ": " + name.charAt(i) + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+
+        for (int n= 0; n < numberOfCheers; n++) {
+            System.out.println(name + "!!!");
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
