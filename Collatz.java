53f8417aef04bd7df80dfc215673e9ed8a0303aa
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-17 13:28:58 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,39 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) {
+
+        int N = Integer.parseInt(args[0]);
+        String verboseOrConcise = args[1];
+
+        if (verboseOrConcise.equals ("v")) {
+
+            for (int i = 1; i <= N; i++) {
+                executeCollatz(i);
+                System.out.println();
+
+            }
+            System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+
+        } else {
+            System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+        }
+    }
+
+
+        public static void executeCollatz (int N) {
+        int stepCount = 1;
+            System.out.print(N + " ");
+
+            while (N != 1) {
+                if (N % 2 == 0) {
+                    N = N / 2;
+                    System.out.print(N + " ");
+                } else {
+                    N = (N * 3) + 1;
+                    System.out.print(N + " ");
+                }
+                stepCount ++;
+            }
+            System.out.print ("(" + stepCount + ")" );
+
+        }
 }
